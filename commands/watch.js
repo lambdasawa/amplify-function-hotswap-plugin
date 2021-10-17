bbc66dd17b5540146798c6f408a4d700753c4f63
Tsubasa Irisawa <lambdasawa@gmail.com>
2021-10-17 20:44:07 +0900

    Add retry process and logging

--- commands/watch.js
+++ commands/watch.js
@@ -8,13 +8,33 @@ const AWS = require("aws-sdk");
 
 const functionDirectoryPath = path.join("amplify", "backend", "function");
 
+const updateRequests = {};
+
 async function run(context) {
-  context.print.info("Start watch...");
-  initAWS();
+  context.print.info("Wait file change...");
+  initAWS(context);
   startWatch(context);
+  startWorker(context)
+    .then(() => {
+      context.print.info("Finish.");
+      process.exit(0);
+    })
+    .catch((e) => {
+      context.print.error(e);
+      process.exit(1);
+    });
 }
 
-function initAWS() {
+async function measure(context, label, promise) {
+  context.print.info(`Start ${label}`);
+  const startTime = Date.now();
+  const result = await promise;
+  const endTime = Date.now();
+  context.print.info(`End ${label} (${(endTime - startTime) / 1000}sec)`);
+  return result;
+}
+
+function initAWS(context) {
   const { envName } = stateManager.getLocalEnvInfo();
   const { profileName } = stateManager.getLocalAWSInfo()[envName];
   AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: profileName });
@@ -27,9 +47,38 @@ function initAWS() {
 function startWatch(context) {
   watch(functionDirectoryPath, { recursive: true }, (event, name) =>
     onFileChange(context, event, name)
-      .then(() => console.log(`Uploaded: ${name}`))
-      .catch((e) => console.error(e))
+      .then(() => context.print.info(`File changed detected: ${name}`))
+      .catch((e) => context.print.error(e))
   );
+}
+
+async function startWorker(context) {
+  while (true) {
+    const resourceNames = Object.keys(updateRequests).filter((resourceName) => updateRequests[resourceName]);
+    if (resourceNames.length > 0) {
+      const resourceName = resourceNames[Math.floor(Math.random() * resourceNames.length)];
+
+      try {
+        if (isResourceFunction(resourceName)) {
+          await measure(context, `UpdateFunction(${resourceName})`, updateLambdaFunction(context, resourceName));
+        } else if (isResourceLayer(resourceName)) {
+          await measure(context, `UpdateLayerAndFunction(${resourceName})`, updateLambdaLayer(context, resourceName));
+        }
+      } catch (e) {
+        if (/Please try this request again/.test(e.message)) {
+          context.print.error(e.message);
+          await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+          continue;
+        } else {
+          throw e;
+        }
+      }
+
+      updateRequests[resourceName] = false;
+    }
+
+    await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+  }
 }
 
 async function onFileChange(context, event, name) {
@@ -39,11 +88,7 @@ async function onFileChange(context, event, name) {
 
   const resourceName = getResourceNameFromPath(name);
 
-  if (isResourceFunction(resourceName)) {
-    await updateLambdaFunction(context, resourceName);
-  } else if (isResourceLayer(resourceName)) {
-    await updateLambdaLayer(context, resourceName);
-  }
+  updateRequests[resourceName] = true;
 }
 
 function getResourceNameFromPath(pathString) {
@@ -65,7 +110,11 @@ function isResourceLayer(resourceName) {
 async function updateLambdaFunction(context, resourceName) {
   const functionName = getFunctionNameFromResourceName(resourceName);
 
-  const zipPath = await createZip(path.join("amplify", "backend", "function", resourceName, "src"));
+  const zipPath = await measure(
+    context,
+    "CreateZip",
+    createZip(path.join("amplify", "backend", "function", resourceName, "src"))
+  );
 
   const updateFunctionCodeParams = {
     FunctionName: functionName,
@@ -73,7 +122,7 @@ async function updateLambdaFunction(context, resourceName) {
   };
 
   const lambda = new AWS.Lambda();
-  await lambda.updateFunctionCode(updateFunctionCodeParams).promise();
+  await measure(context, "UpdateFunctoinCode", lambda.updateFunctionCode(updateFunctionCodeParams).promise());
 }
 
 function getFunctionNameFromResourceName(resourceName) {
@@ -85,29 +134,39 @@ function getFunctionNameFromResourceName(resourceName) {
 async function updateLambdaLayer(context, resourceName) {
   const { name: layerName, version: layerVersion } = parseLayerArn(resourceName);
 
-  const zipPath = await createZip(path.join("amplify", "backend", "function", resourceName), { isFunction: false });
+  const zipPath = await measure(
+    context,
+    "CreateZip",
+    createZip(path.join("amplify", "backend", "function", resourceName), {
+      isFunction: false,
+    })
+  );
 
   const lambda = new AWS.Lambda();
-  const { LayerVersionArn: layerVersionArn } = await lambda
-    .publishLayerVersion({
-      LayerName: layerName,
-      CompatibleRuntimes: require(path.join(
-        process.cwd(),
-        "amplify",
-        "backend",
-        "function",
-        resourceName,
-        "parameters.json"
-      )).runtimes,
-      Content: {
-        ZipFile: fs.readFileSync(zipPath),
-      },
-    })
-    .promise();
+  const { LayerVersionArn: layerVersionArn } = await measure(
+    context,
+    "PublishLayerVersion",
+    lambda
+      .publishLayerVersion({
+        LayerName: layerName,
+        CompatibleRuntimes: require(path.join(
+          process.cwd(),
+          "amplify",
+          "backend",
+          "function",
+          resourceName,
+          "parameters.json"
+        )).runtimes,
+        Content: {
+          ZipFile: fs.readFileSync(zipPath),
+        },
+      })
+      .promise()
+  );
 
   await Promise.all(
     findReferencedFunctionNames(resourceName).map((functionName) =>
-      updateLayerDependency(functionName, layerVersionArn)
+      updateLayerDependency(context, functionName, layerVersionArn)
     )
   );
 }
@@ -130,22 +189,30 @@ function findReferencedFunctionNames(resourceName) {
     .map((arn) => arn.match(/arn:aws:lambda:.*:[\d]*:function:([^:]*)/)[1]);
 }
 
-async function updateLayerDependency(functionName, layerVeresionArn) {
+async function updateLayerDependency(context, functionName, layerVeresionArn) {
   const lambda = new AWS.Lambda();
 
-  const currentConfiguration = await lambda.getFunctionConfiguration({ FunctionName: functionName }).promise();
+  const currentConfiguration = await measure(
+    context,
+    "GetFunctoinConfiguration",
+    lambda.getFunctionConfiguration({ FunctionName: functionName }).promise()
+  );
 
   const currentLayersWithoutOld = currentConfiguration.Layers.map(({ Arn }) => Arn).filter(
     (arn) => !arn.startsWith(layerVeresionArn.replace(/:[\d]+$/, ""))
   );
   const newLayers = [...currentLayersWithoutOld, layerVeresionArn];
 
-  await lambda
-    .updateFunctionConfiguration({
-      FunctionName: functionName,
-      Layers: newLayers,
-    })
-    .promise();
+  await measure(
+    context,
+    "UpdateFunctionConfiguration",
+    lambda
+      .updateFunctionConfiguration({
+        FunctionName: functionName,
+        Layers: newLayers,
+      })
+      .promise()
+  );
 }
 
 async function createZip(dir, option = { isFunction: true }) {
